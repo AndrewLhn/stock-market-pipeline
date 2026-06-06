c54bcae1f211e5ba0dedf55d1478d051367e35e1
Andrej Zhyvitko <your.email@example.com>
2026-06-06 23:29:41 +0300

    create dbt models

--- dbt_project/models/staging/stg_stocks.sql
+++ dbt_project/models/staging/stg_stocks.sql
@@ -1,18 +1,16 @@
 {{ config(materialized='view') }}
 
 with raw_data as (
-    select *
-    from read_csv_auto('s3://stock-market-data/raw/**/*.csv')
+    select * from read_csv_auto('s3://stock-market-data/raw/**/*.csv')
 )
 
 select
-    cast(Date as date) as trading_date,
-    cast(ticker as varchar) as ticker,
-    cast(Open as float) as open_price,
-    cast(High as float) as high_price,
-    cast(Low as float) as low_price,
-    cast(Close as float) as close_price,
-    cast("Adj Close" as float) as adj_close_price,
-    cast(Volume as bigint) as volume
+    try_cast(Date as DATE) as trading_date,
+    upper(ticker) as ticker,
+    cast(Open as DOUBLE) as open_price,
+    cast(High as DOUBLE) as high_price,
+    cast(Low as DOUBLE) as low_price,
+    cast(Close as DOUBLE) as close_price,
+    cast(Volume as BIGINT) as volume
 from raw_data
-where ticker is not null and Date is not null
+where Date is not null and Date != 'Date'
